df1824f08e8a4c31b3a45f8cab4303586e3dbc58
Dmytro <dg318842@gmail.com>
2026-09-01 10:35:07 +0200

    Fixed the look of card info on phones and tablets

---
+++ frontend/public/scripts/test.js
@@ -0,0 +1,9 @@
+//creating a variable
+let age = 35;
+let year = 2026;
+//print things to the console
+console.log(age, year);
+age = 30;
+console.log(age);
+const name = "Dima";
+console.log(name);
